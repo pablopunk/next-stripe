5410380912796d03dcc39d194f6c4a53b8bad051
Pablo P Varela <pablo@pablopunk.com>
2021-06-04 03:10:32 +0200

    fix env

--- constants/url.ts
+++ constants/url.ts
@@ -2,5 +2,5 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const SITE_URL = dev
   ? 'http://localhost:3000'
-  : 'https://' + process.env.VERCEL_URL
+  : 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
 export const API_URL = SITE_URL + '/api'
