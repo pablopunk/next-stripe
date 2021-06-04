c73e1c9a714020247096cb995b5f64981cedbd38
Pablo P Varela <pablo@pablopunk.com>
2021-06-04 03:08:13 +0200

    fix vercel url

--- constants/url.ts
+++ constants/url.ts
@@ -1,4 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const SITE_URL = dev ? 'http://localhost:3000' : process.env.VERCEL_URL
+export const SITE_URL = dev
+  ? 'http://localhost:3000'
+  : 'https://' + process.env.VERCEL_URL
 export const API_URL = SITE_URL + '/api'
